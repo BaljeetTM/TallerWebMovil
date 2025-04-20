0ce5372cd9ee768f41110ea6b2164e97f0e34bbf
BaljeetTM <nicolas.tapia08@alumnos.ucn.cl>
2025-04-20 16:42:43 -0400

    feat: add Product and StoreContext

---
+++ APITaller1/src/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using APITaller1.src.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace APITaller1.src.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Product> Products { get; set; }
+           
+    }
+
+}
