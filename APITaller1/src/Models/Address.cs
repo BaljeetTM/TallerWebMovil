d8f99b1739ab22c0e6225a4fecc2932c63610002
BaljeetTM <nicolas.tapia08@alumnos.ucn.cl>
2025-04-20 20:57:58 -0400

    feat: add product atributes

--- APITaller1/src/Models/Address.cs
+++ APITaller1/src/Models/Address.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 namespace APITaller1.src.Models
 {
     public class Address
-{
-    public int Id { get; set; }
-    public required string Street { get; set; }
-    public required string Number { get; set; }
-    public required string Commune { get; set; }
-    public required string Region { get; set; }
-    public required string PostalCode { get; set; }
-    public bool IsDefault { get; set; } = true;
+    {
+        public int Id { get; set; }
+        public required string Street { get; set; }
+        public required string Number { get; set; }
+        public required string Commune { get; set; }
+        public required string Region { get; set; }
+        public required string PostalCode { get; set; }
+        public bool IsDefault { get; set; } = true;
 
-    public int UserId { get; set; }
-    public User? User { get; set; }
-}
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
 }
