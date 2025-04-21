651909841188a9c1af618b8fc61af1910805743f
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-04-20 22:22:37 -0400

    feat: add database update

--- APITaller1/Program.cs
+++ APITaller1/Program.cs
@@ -1,19 +1,21 @@
 using APITaller1.src.Data;
-
 using Microsoft.EntityFrameworkCore;
-
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
-
     .CreateLogger();
+
 try
 {
     Log.Information("starting server.");
+
     var builder = WebApplication.CreateBuilder(args);
+
     builder.Services.AddControllers();
+
     builder.Services.AddDbContext<StoreContext>(options =>
         options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
     builder.Host.UseSerilog((context, services, configuration) =>
     {
         configuration
@@ -21,11 +23,18 @@ try
             .Enrich.FromLogContext()
             .Enrich.WithThreadId()
             .Enrich.WithMachineName();
-
     });
 
     var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<StoreContext>();
+        db.Database.Migrate();
+    }
+
     DbInitializer.InitDb(app);
+
     app.MapControllers();
     app.Run();
 }
