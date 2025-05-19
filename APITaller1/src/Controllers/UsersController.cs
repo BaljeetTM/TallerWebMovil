6f6f6524f8955510597d3ae40764c60019e0cdde
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-05-18 21:35:44 -0400

    feat: add UsersController CRUD

--- APITaller1/src/Controllers/UsersController.cs
+++ APITaller1/src/Controllers/UsersController.cs
@@ -31,4 +31,64 @@ public class UsersController : ControllerBase
 
         return Ok(userDtos);
     }
+
+    [HttpGet("{id}")] // /api/users/1
+    public async Task<ActionResult<UserDto>> GetUser(int id)
+    {
+        var user = await _context.Users
+            .Include(u => u.Role) // Muy importante para que se cargue Role
+            .FirstOrDefaultAsync(u => u.Id == id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userDto = _mapper.Map<UserDto>(user);
+
+        return Ok(userDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserDto>> CreateUser(CreateUserDto createUserDto)
+    {
+        var user = _mapper.Map<User>(createUserDto);
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+    }
+
+    [HttpPut("{id}")] // /api/users/1
+    public async Task<IActionResult> UpdateUser(int id, UpdateUserDto updateUserDto)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _mapper.Map(updateUserDto, user);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")] // /api/users/1
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
 }
